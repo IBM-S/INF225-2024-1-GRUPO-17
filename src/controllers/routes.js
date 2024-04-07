4551865ce89f383e64512f472b9c4ec0f0ec5b8e
alessandro.cintolesi <alessandro.cintolesi@alumnos.inf.utfsm.cl>
2024-04-07 14:19:46 -0400

    Mensaje del commit

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -1,1 +1,9 @@
-export default (app) => {};
+import PacienteController from './pacienteController.js';
+
+export default (app) => {
+    const pacienteController = new PacienteController();
+
+	app.get('/allPacientes', pacienteController.getAllPacientes);
+	app.post('/loginPaciente', pacienteController.loginPaciente);
+	app.post('/registerPaciente', pacienteController.registerPaciente);
+};
